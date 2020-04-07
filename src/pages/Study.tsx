281da0c9b9d8cb68ef54b6ffaf63e2f3d39f1c2b
XingFengShuo <xingfengshuo.leo@gmail.com>
2020-04-07 19:53:25 +0800

    feat(study): 新增 - 写作页导航(提交|模式|等等)
    新增: 写作页导航(提交|模式|等等)

--- src/pages/Study.tsx
+++ src/pages/Study.tsx
@@ -2,9 +2,20 @@ import React from "react";
 import marked from "marked";
 import CodeMirror from "codemirror";
 import Prism from "prismjs";
+import Navigation from "component/Navigation";
 
 import "codemirror/keymap/vim";
+// 使用语言
+import "codemirror/mode/python/python";
 import "codemirror/mode/markdown/markdown";
+import "codemirror/mode/htmlmixed/htmlmixed";
+import "codemirror/mode/javascript/javascript";
+
+// 代码折叠
+import "codemirror/addon/fold/brace-fold";
+import "codemirror/addon/fold/foldcode";
+import "codemirror/addon/fold/foldgutter";
+import "codemirror/addon/fold/foldgutter.css";
 
 // 需要做按需加载
 import "prismjs/themes/prism.css";
@@ -44,66 +55,151 @@ marked.setOptions({
   }
 });
 
+enum EditModeType {
+  md = "markdown",
+  js = "javascript",
+  py = "python",
+  html = "htmlmixed"
+}
+
+enum ViewModeType {
+  "编辑模式",
+  "预览模式"
+}
 export default class Study extends React.Component<P, S> {
-  textareaRef: React.RefObject<any>;
+  editor: any;
+
+  textareaPRef: React.RefObject<any>;
 
   constructor(props: Readonly<P>) {
     super(props);
     this.state = {
-      content: ""
+      content: localStorage.getItem("studyContent") || "",
+      editMode: EditModeType.md,
+      viewMode: ViewModeType.编辑模式
     };
-    this.textareaRef = React.createRef();
+    this.textareaPRef = React.createRef();
   }
 
   componentDidMount() {
-    const { content } = this.state;
-    const textareaRef = this.textareaRef.current;
-    const editor = CodeMirror(
+    this.initCodeMirror();
+  }
+
+  componentDidUpdate(preProps: P, nextState: S) {
+    const { editMode } = this.state;
+    if (editMode !== nextState.editMode) {
+      this.initCodeMirror();
+    }
+  }
+
+  initCodeMirror = () => {
+    const { content, editMode } = this.state;
+    const textareaPRef = this.textareaPRef.current;
+    this.editor = CodeMirror(
       ele => {
-        textareaRef.parentNode.replaceChild(ele, textareaRef);
+        textareaPRef.replaceChild(ele, textareaPRef.children[0]);
       },
       {
+        indentUnit: 4,
         value: content,
         theme: "material",
         keyMap: "vim",
-        mode: {
-          name: "markdown",
-          json: true
-        },
+        mode: editMode,
         lineNumbers: true,
-        lineWrapping: true
+        lineWrapping: true,
+        foldGutter: true, // 折叠代码
+        gutters: ["CodeMirror-linenumbers", "CodeMirror-foldgutter"]
       }
     );
-    editor.on("change", () => {
-      const val = editor.getValue();
+    this.editor.on("change", () => {
+      const val = this.editor.getValue();
       this.setState({
         content: val
       });
+      localStorage.setItem("studyContent", val);
     });
-  }
+  };
 
   getMarkdownText = (value: string) => {
     return marked(value);
   };
 
-  onEdit = () => {
-    //
+  onTabViewMode = () => {
+    const { viewMode } = this.state;
+    const val = viewMode ? 0 : 1;
+    this.setState({
+      viewMode: val
+    });
+  };
+
+  onEditMode = (event: any) => {
+    this.setState({
+      editMode: event.target.value
+    });
+  };
+
+  onClean = () => {
+    this.editor.setValue("");
+  };
+
+  onSubmit = () => {
+    // 接口
+    localStorage.removeItem("studyContent");
   };
 
   render() {
-    const { content } = this.state;
+    const str = "```";
+    const { content, viewMode, editMode } = this.state;
+    const btnStyle =
+      "outline-none mr-10 py-4 appearance-none bg-transparent text-center text-center-last cursor-pointer hover:text-jc-hover-color";
+    const wrapStyle = " overflow-y-scroll flex-1";
+    const editStyle = `codemirror-body ${wrapStyle}`;
+    const markStyle = `markdown-body ${wrapStyle} ${ViewModeType[viewMode] === "编辑模式" ? "" : " hidden"}`;
+    const markContent = editMode === EditModeType.md ? content : `${str + EditModeType.md}\n${content}\n${str}`;
+
     return (
-      <>
-        <div>{/* <button type="button">提交</button> */}</div>
-        <div className="flex border h-screen border-gray-100">
-          <div className="codemirror-body flex-1 ">
-            <textarea ref={this.textareaRef} value={content} onChange={this.onEdit} />
+      <div className="flex flex-col h-screen">
+        <Navigation />
+
+        <div className="flex justify-end font-mono text-l text-jc-text-color border-b-2 border-gray-200">
+          <span className={btnStyle}>字数</span>
+          <span className={btnStyle}>耗时</span>
+          <button type="button" className={btnStyle} onClick={this.onClean}>
+            清空内容
+          </button>
+          <button type="button" className={btnStyle}>
+            同步滚动
+          </button>
+          <select className={btnStyle}>
+            <option>MATERIAL</option>
+          </select>
+          <select className={btnStyle} value={editMode} onChange={this.onEditMode}>
+            {Object.keys(EditModeType).map((item: string) => {
+              const detail = (EditModeType as any)[item];
+              return (
+                <option key={item} value={detail}>
+                  {detail.toUpperCase()}
+                </option>
+              );
+            })}
+          </select>
+          <button type="button" className={btnStyle} onClick={this.onTabViewMode}>
+            {ViewModeType[viewMode]}
+          </button>
+          <button type="button" className={btnStyle} onClick={this.onSubmit}>
+            提交
+          </button>
+        </div>
+
+        <div className="flex flex-1 overflow-hidden">
+          <div className={`${editStyle}`} ref={this.textareaPRef}>
+            <textarea />
           </div>
-          <div className="markdown-body overflow-y-scroll flex-1">
-            <div dangerouslySetInnerHTML={{ __html: marked(content) }} />
+          <div className={markStyle}>
+            <div dangerouslySetInnerHTML={{ __html: marked(markContent) }} />
           </div>
         </div>
-      </>
+      </div>
     );
   }
 }
@@ -111,4 +207,6 @@ export default class Study extends React.Component<P, S> {
 type P = {};
 type S = {
   content: string;
+  viewMode: number;
+  editMode: string;
 };
