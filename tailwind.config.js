c87f08e30decb8248419097cb244de08d05e1876
XingFengShuo <xingfengshuo.leo@gmail.com>
2020-05-13 21:18:54 +0800

    ci(script): 更改 - 移出add所有文件 | 增加样式配置
    更改: 移出add所有文件 | 增加样式配置

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,26 +4,31 @@ module.exports = {
       colors: {
         "jc-bg-color": "#F7F2F0",
         "jc-hover-color": "#c5b8a8",
-        "jc-text-color": "#73736f"
+        "jc-text-color": "#73736f",
       },
       height: {
-        "screen-50": "50vh"
+        "screen-50": "50vh",
+      },
+      width: {
+        "screen-50": "50vw",
+        "screen-40": "40vw",
+        "screen-30": "30vw",
       },
       maxWidth: {
         "2/12": "16.666667%;",
         "screen-xl": "1280px",
         "screen-lg": "1024px",
         "screen-md": "768px",
-        "screen-sm": "640px"
+        "screen-sm": "640px",
       },
       minWidth: {
-        "jc-300": "300px"
+        "jc-300": "300px",
       },
       borderRadius: {
-        "jc-3": "3rem"
-      }
-    }
+        "jc-3": "3rem",
+      },
+    },
   },
   variants: {},
-  plugins: []
+  plugins: [],
 };
